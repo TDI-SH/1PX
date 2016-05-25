8183587dc912959a2aaf1c0383cc1d1b78e7dd9f
robberfree <RWU@CNSGHMD008.local>
2016-05-25 18:32:58 +0800

    update index.html

--- src/js/common.js
+++ src/js/common.js
@@ -2,6 +2,8 @@
 {
     console.log('common.js start');
     
+    var headerHeight=78;
+    
     var btnMenuOpen=document.querySelector('.menu__btnMenuOpen');
     var btnMenuClose=document.querySelector('.menu__btnMenuClose');
     var menuOpen=document.querySelector('.menu__open');
@@ -11,12 +13,18 @@
     
     function handleMenuOpen(e)
     {
-        menuOpen.style.display='block';        
+        menuOpen.style.display='block';
+        TweenLite.set(menuOpen,{height:headerHeight});
+        TweenLite.to(menuOpen,0.3,{height:'100%',ease:'Ease.easeOut'});       
     }
     
     function handleMenuClose(e)
     {
+        TweenLite.to(menuOpen,0.3,{height:headerHeight,ease:'Ease.easeIn',onComplete:handleComplete});                              
+    }
+    
+    function handleComplete()
+    {
         menuOpen.style.display='none';        
     }    
-    
 })();
